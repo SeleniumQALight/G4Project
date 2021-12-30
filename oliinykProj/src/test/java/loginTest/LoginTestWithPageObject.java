0ae87660e77bf7c45ce3a4abd4549462932b5256
Andrii <livecounter86@gmail.com>
2021-12-30 21:30:30 +0200

    Case made with pageobject

---
+++ oliinykProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -0,0 +1,17 @@
+package loginTest;
+
+import baseTest.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginTestWithPageObject extends BaseTest {
+    @Test
+    public void validLogin(){
+        loginPage.openLoginPage();
+        loginPage.enterLoginIntoInputLogin("qaauto");
+        loginPage.enterPasswordIntoInputPassword("123456qwerty");
+        loginPage.clickOnSignIn();
+
+        Assert.assertTrue("Button Sign Out went for wecetion", homePage.dispayedButtonSignOut());
+    }
+}
