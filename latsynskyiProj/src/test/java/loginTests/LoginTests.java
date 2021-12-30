5e1af3a4f16b56326780b338fbe0625d112ed3d8
Latsynskyi <latsynskyi@gmail.com>
2021-12-30 21:32:31 +0200

    test with page object

--- latsynskyiProj/src/test/java/loginTests/LoginTests.java
+++ latsynskyiProj/src/test/java/loginTests/LoginTests.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class loginTests {
+public class LoginTests {
     WebDriver webDriver;
     @Test
 public void validLogin(){
