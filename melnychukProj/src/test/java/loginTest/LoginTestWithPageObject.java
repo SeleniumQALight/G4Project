93312a1a97f4bf45b8942af93986d513b351e205
omelaT <olha.melnichuk@gmail.com>
2022-01-03 21:18:40 +0200

    Home Work 2 and Home Work 3 ( invalid login tests)

--- melnychukProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ melnychukProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -13,8 +13,16 @@ public class LoginTestWithPageObject extends BaseTest {
         loginPage.enterPasswordIntoInputPassword("123456qwerty");
         loginPage.clickOnButtonSignIn();
 
-        Assert.assertTrue("Button signOut is NOT displayed ", homePage.isButtonSignOutDisplayed());
+        Assert.assertTrue("Button signOut is displayed ", homePage.isButtonSignOutDisplayed());
+    }
+    @Test
+    public void inValidLoginTest(){
+        loginPage.openLoginPage();
+        loginPage.enterLoginIntoInputLogin("qaauto");
+        loginPage.enterPasswordIntoInputPassword("123456qwerty123");
+        loginPage.clickOnButtonSignIn();
 
+        Assert.assertTrue("Button signOut is NOT displayed, signIn displayed ", homePage.isButtonSignInDisplayed());
 
     }
 }
