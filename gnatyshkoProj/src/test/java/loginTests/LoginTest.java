6423f1826bbb54afac4697bd1dfe7a45a554f423
admin <gnatyshko.i@gmail.com>
2022-01-03 12:30:40 +0100

    test 'invalidLogin' added to LoginTest (without PageObject Pattern)

--- gnatyshkoProj/src/test/java/loginTests/LoginTest.java
+++ gnatyshkoProj/src/test/java/loginTests/LoginTest.java
@@ -43,6 +43,35 @@ public class LoginTest {
         System.out.println("Browser was closed");
     }
 
+    @Test
+    public void invalidLogin() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+
+        webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
+        System.out.println("Site was open");
+
+        webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']")).clear();
+        webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"))
+                .sendKeys("qa");
+        System.out.println("Login was entered");
+
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']")).clear();
+        webDriver.findElement(By.xpath(".//input[@placeholder='Password']"))
+                .sendKeys("123456qwerty");
+        System.out.println("Password was entered");
+
+        webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
+        System.out.println("Button was clicked");
+
+        Assert.assertFalse("Button Sign Out is displayed"
+                , isButtonSignOutDisplayed());
+
+        webDriver.quit();
+        System.out.println("Browser was closed");
+    }
 
     private boolean isButtonSignOutDisplayed() {
         try {
