7be994db6535c1dbd5bcbfaff8ca201b34210030
Nadiia Moiseieva <nadezhda.moiseeva@ukr.net>
2022-01-04 21:46:18 +0200

    moiseieva - methods were added in ParenPage

--- moiseievaProj/src/main/java/pages/ParentPage.java
+++ moiseievaProj/src/main/java/pages/ParentPage.java
@@ -1,7 +1,10 @@
 package pages;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class ParentPage {
     Logger logger = Logger.getLogger(getClass());
@@ -9,5 +12,30 @@ public class ParentPage {
 
     public ParentPage(WebDriver webDriver) {
         this.webDriver = webDriver;
+        PageFactory.initElements(webDriver,this);
+    }
+
+    protected void enterTextToElement(WebElement element, String text){
+        try {
+            element.clear();
+            element.sendKeys(text);
+            logger.info(text + " was inputted " );
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    protected void clickOnElement(WebElement element){
+        try {
+            element.click();
+            logger.info("Element was clicked");
+        } catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
+    private void printErrorAndStopTest(Exception e) {
+        logger.error("Can not work with element " + e);
+        Assert.fail("Can not work with element " + e);
     }
 }
