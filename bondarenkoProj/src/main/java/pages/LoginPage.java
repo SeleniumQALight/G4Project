65096b81ad33b49ea15890ffb4251606cd6fb975
AnBondarenko <bondarenko.nastya86@gmail.com>
2022-01-06 15:25:32 +0200

    3rd homework

--- bondarenkoProj/src/main/java/pages/LoginPage.java
+++ bondarenkoProj/src/main/java/pages/LoginPage.java
@@ -16,16 +16,37 @@ public class LoginPage extends ParentPage {
     @FindBy(xpath = ".//button[text()='Sign In']")
     private WebElement buttonSignIn;
 
+    @FindBy(xpath = ".//*[@id='username-register']")
+    private WebElement inputUsernameSignUp;
+
+    @FindBy(xpath = ".//*[@id='email-register']")
+    private WebElement inputEmailSignUp;
+
+    @FindBy(xpath = ".//*[@id='password-register']")
+    private WebElement inputPasswordSignUp;
+
+    @FindBy(xpath = ".//*[@type='submit']")
+    private WebElement buttonSignUpForOutApp;
+
+    @FindBy(xpath = ".//div[text()='Username must be at least 3 characters.']")
+    private WebElement messageForUsernameSignUpField;
+
+    @FindBy(xpath = ".//div[text()='You must provide a valid email address.']")
+    private WebElement messageForEmailSignUpField;
+
+    @FindBy(xpath = ".//div[text()='Password must be at least 12 characters.']")
+    private WebElement messageForPasswordSignUpFiled;
+
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
 
-    public void openLoginPage(){
+    public void openLoginPage() {
         try {
             webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
             logger.info("Login page was opened");
-        } catch (Exception e){
-          logger.error("Can not open Login Page" + e);
+        } catch (Exception e) {
+            logger.error("Can not open Login Page" + e);
             Assert.fail("Can not open Login Page" + e);
         }
 
@@ -42,7 +63,7 @@ public class LoginPage extends ParentPage {
         enterTextIntoElement(inputLoginSignIn, login);
     }
 
-    public void enterPasswordIntoInputPassword(String passWord){
+    public void enterPasswordIntoInputPassword(String passWord) {
 //        try{
 //            inputPassWordSignIn.clear();
 //            inputPassWordSignIn.sendKeys(passWord);
@@ -63,11 +84,70 @@ public class LoginPage extends ParentPage {
         clickOnElement(buttonSignIn);
     }
 
-    public HomePage loginWithValidCred(){
+    public HomePage loginWithValidCred() {
         openLoginPage();
         enterLoginIntoInputLogin(TestData.VALID_LOGIN);
         enterPasswordIntoInputPassword(TestData.VALID_PASS);
         clickOnButtonSignIn();
         return new HomePage(webDriver);
     }
+
+    public void enterUsernameIntoInputUsernameSignUp(String username) {
+        enterTextIntoElement(inputUsernameSignUp, username);
+    }
+
+    public void enterEmailIntoInputEmailSignUp(String email) {
+        enterTextIntoElement(inputEmailSignUp, email);
+
+    }
+
+    public void enterPasswordIntoInputPasswordSignUp(String password) {
+        enterTextIntoElement(inputPasswordSignUp, password);
+    }
+
+    public void clickOnButtonSignUpForOurApp() {
+        clickOnElement(buttonSignUpForOutApp);
+    }
+
+    public boolean isMessageForUsernameSignUpFieldDisplayed(){
+        try {
+            return messageForUsernameSignUpField.isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public LoginPage checkIsMessageForUsernameSignUpFieldDisplayed(){
+        Assert.assertTrue("Message for Username field is not displayed", isMessageForUsernameSignUpFieldDisplayed());
+        return this;
+    }
+
+    public boolean isMessageForEmailSignUpFieldDisplayed(){
+        try {
+            return messageForEmailSignUpField.isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public LoginPage checkIsMessageForEmailSignUpFieldDisplayed(){
+        Assert.assertTrue("Message for Email field is not displayed", isMessageForEmailSignUpFieldDisplayed());
+        return this;
+    }
+
+    public boolean isMessageForPasswordSignUpFieldDisplayed(){
+        try {
+            return messageForPasswordSignUpFiled.isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public LoginPage checkIsMessageForPasswordSignUpFieldDisplayed(){
+        Assert.assertTrue("Message for Password field is not displayed", isMessageForPasswordSignUpFieldDisplayed());
+        return this;
+    }
+
 }
+
+
---
+++ bondarenkoProj/src/test/java/registrationTests/RegistrationWithInvalidDataTest.java
@@ -0,0 +1,19 @@
+package registrationTests;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class RegistrationWithInvalidDataTest extends BaseTest {
+    @Test
+    public  void enterInvalidData(){
+        loginPage.openLoginPage();
+        loginPage.enterUsernameIntoInputUsernameSignUp("tr");
+        loginPage.enterEmailIntoInputEmailSignUp("test.com");
+        loginPage.enterPasswordIntoInputPasswordSignUp("123");
+        loginPage.clickOnButtonSignUpForOurApp();
+        loginPage.checkIsMessageForUsernameSignUpFieldDisplayed();
+        loginPage.checkIsMessageForEmailSignUpFieldDisplayed();
+        loginPage.checkIsMessageForPasswordSignUpFieldDisplayed();
+
+    }
+}
