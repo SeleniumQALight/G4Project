49071461cef3828bd6909cdb44512e5060869dfe
Yuriy-Kuschch <kushchuriy@gmail.com>
2022-01-10 00:34:31 +0200

    test with page object 30-12

---
+++ kuschchProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -0,0 +1,19 @@
+package loginTests;
+
+import baseTest.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginTestWithPageObject extends BaseTest {
+    @Test
+    public void validLoginTest(){
+        loginPage.openLoginPage();
+        loginPage.enterLoginIntoInputLogin("qaauto");
+        loginPage.enterPassWordIntoInputPassWord("123456qwerty");
+        loginPage.clickOnButtonSingIn();
+
+        Assert.assertTrue("Button Sign Out is not displayed"
+                , homePage.isButtonSignOutDisplayed());
+    }
+
+}
