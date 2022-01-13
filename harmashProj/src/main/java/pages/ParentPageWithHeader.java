6ed934806ea93a8836184ff1e15319e4b2c027e8
Faina Harmash <faya.garmash.work@gmail.com>
2022-01-13 20:26:42 +0200

    Added waits, changes void to create post page

--- harmashProj/src/main/java/pages/ParentPageWithHeader.java
+++ harmashProj/src/main/java/pages/ParentPageWithHeader.java
@@ -4,14 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class ParentPageWithHeader extends ParentPage{
+public class ParentPageWithHeader extends ParentPage {
     @FindBy(xpath = ".//a[text()='Create Post']")
     private WebElement buttonCreatePost;
 
     public ParentPageWithHeader(WebDriver webDriver) {
         super(webDriver);
     }
-    public void clickOnCreatePostButton(){
+
+    public CreatePostPage clickOnCreatePostButton() {
+
         clockOnElement(buttonCreatePost);
+        return new CreatePostPage(webDriver);
     }
 }
