8d6dccfe204a7f46cd8ede77b18648e4c4e1b8a4
Yuriy-Kuschch <kushchuriy@gmail.com>
2022-01-13 21:46:12 +0200

    Util was added

--- kuschchProj/src/test/java/postsTest/CreatePostTest.java
+++ kuschchProj/src/test/java/postsTest/CreatePostTest.java
@@ -1,24 +1,30 @@
 package postsTest;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreatePostTest extends BaseTest {
+    final String title = "G4-kuschch: 1-13" + Util.getDateAndTimeFormatted();
+
     @Test
     public void createNewPost(){
+
         loginPage
                 .loginWithValidCred()
                 .checkIsButtonSignOutDisplayed()
                 .clickOnCreatePostButton();
         createPostPage
                 .checkIsRedirectToCreatePostPage()
-                .enterTextIntoTitleInput("G4-kuschch")
+                .enterTextIntoTitleInput(title)
                 .enterTextIntoBody("Body post")
 //                .selectTextInDropDownRole("Частное сообщение")
                 .selectValueDropdownRole("One Person")
                 .clickOnButtonSaveNewPost()
                 .checkIsRedirectToPostPage()
                 .checkTestInAlert("New post successfully created.")
+                .clickOnMyProfileButton()
+                .checkPostWasCreated(title)
         ;
     }
 }
