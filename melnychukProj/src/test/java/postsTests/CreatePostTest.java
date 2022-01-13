f4d85cab71bbdf3fa01017e841c72832629aab75
omelaT <olha.melnichuk@gmail.com>
2022-01-13 21:45:52 +0200

    util added

--- melnychukProj/src/test/java/postsTests/CreatePostTest.java
+++ melnychukProj/src/test/java/postsTests/CreatePostTest.java
@@ -1,21 +1,27 @@
 package postsTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreatePostTest  extends BaseTest{
+   final String title  = "G4-OlhaM 1-13" + Util.getDateAndTimeFormatted();
     @Test
     public  void createNewPost(){
+
+
         loginPage.loginWithValidCred().checkIsButtonSignOutDisplayed()
                 .clickOnCreatePostButton();
         createPostPage.checkIsredirectToCreatePostPage()
-                .enterTextInToTitleInput("G4-OlhaM")
+                .enterTextInToTitleInput(title)
                 .enterTextInToBodyInput("Body post")
                 // .selectTextInDropDownRole("Частное сообщение")
                 .selectValueInDropDownRole("One Person")
                 .clickOnButtonSaveNewPost()
                 .checkIsRedirectToPostPage()
-                .checkTextInAlert("New post successfully created.");
+                .checkTextInAlert("New post successfully created.")
+        .clickOnMyProfileButton()
+        . checkPostWasCreated(title);
     }
 
 
