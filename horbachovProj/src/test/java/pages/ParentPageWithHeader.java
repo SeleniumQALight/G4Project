fd9c8595305642c30bd3f547e281ec74da51e529
Сергій Горбачов <fastmotionsg@gmail.com>
2022-01-13 20:31:24 +0200

    pom+waits

--- horbachovProj/src/test/java/pages/ParentPageWithHeader.java
+++ horbachovProj/src/test/java/pages/ParentPageWithHeader.java
@@ -14,7 +14,9 @@ public class ParentPageWithHeader extends ParentPage{
         super(webDriver);
     }
 
-    public void clickOnCreatePostButton(){
+    public CreatePostPage clickOnCreatePostButton(){
         clickOnElement(buttonCreatePost);
+        return new CreatePostPage(webDriver);
     }
+    
 }
