9e13136e291ea9b859e6a3be83d9936643ec55f5
Livecounter86 <livecounter@i.ua>
2022-01-13 21:46:54 +0200

    Util was added and check on My Profile

--- oliinykProj/src/main/java/pages/ParrentPageWithHeader.java
+++ oliinykProj/src/main/java/pages/ParrentPageWithHeader.java
@@ -8,11 +8,20 @@ public class ParrentPageWithHeader extends ParentPage{
     @FindBy(xpath = ".//a[text()='Create Post']")
     private WebElement buttonCreatePost;
 
+    @FindBy(xpath = ".//*[@data-original-title='My Profile']")
+    private WebElement buttonMyProfile;
+
     public ParrentPageWithHeader(WebDriver driver) {
         super(driver);
     }
 
     public CreatePostPage clickOnCreatePostButton(){
-        clickOnElement(buttonCreatePost); return new CreatePostPage(driver);
+        clickOnElement(buttonCreatePost);
+        return new CreatePostPage(driver);
+    }
+
+    public MyProfilePage clickOnMyProfileButton() {
+        clickOnElement(buttonMyProfile);
+        return new MyProfilePage(driver);
     }
 }
--- oliinykProj/src/test/java/postsTest/CreatePost.java
+++ oliinykProj/src/test/java/postsTest/CreatePost.java
@@ -1,22 +1,27 @@
 package postsTest;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 
 public class CreatePost extends BaseTest {
+    final String title = "G4-Oliinyk " + Util.getDateAndTimeFormatted();
+
     @Test
     public void createNewPost(){
         loginPage.logedInHomepage()
                 .checkIsButtonSingOutDisplayed()
                 .clickOnCreatePostButton();
         createPostPage.checkRedirectToCreatePostPage()
-                .enterTextIntoTitleInput("G4-Oliinyk")
+                .enterTextIntoTitleInput(title)
                 .enterTextIntoBodyInput("Test Auto")
                 //.selectTextInDropDownRole("Частное сообщение")
                 .selectValueInDropdownRole("One Person")
                 .clickOnButtonSaveNewPost()
                 .checkIsRedirectToPostPage()
-                .checkTextInAlert("New post successfully created.");
+                .checkTextInAlert("New post successfully created.")
+                .clickOnMyProfileButton()
+                .checkPostWasCreated(title);
     }
 }
