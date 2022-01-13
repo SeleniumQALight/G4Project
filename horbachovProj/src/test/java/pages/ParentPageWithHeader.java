e5f3271bba3e91aa65ff5afe069d8f4209071404
Сергій Горбачов <fastmotionsg@gmail.com>
2022-01-13 21:53:00 +0200

    create post test phase 2

--- horbachovProj/src/test/java/pages/ParentPageWithHeader.java
+++ horbachovProj/src/test/java/pages/ParentPageWithHeader.java
@@ -9,6 +9,9 @@ public class ParentPageWithHeader extends ParentPage{
     @FindBy(xpath = ".//a[text()='Create Post']")
     private WebElement buttonCreatePost;
 
+    @FindBy(xpath = ".//*[@data-original-title='My Profile']")
+    private WebElement buttonMyProfile;
+
 
     public ParentPageWithHeader(WebDriver webDriver) {
         super(webDriver);
@@ -18,5 +21,11 @@ public class ParentPageWithHeader extends ParentPage{
         clickOnElement(buttonCreatePost);
         return new CreatePostPage(webDriver);
     }
-    
+
+    public MyProfilePage clickOnMyProfileButton(){
+        clickOnElement(buttonMyProfile);
+        return new MyProfilePage(webDriver);
+
+    }
+
 }
--- horbachovProj/src/test/java/postsTest/CreatePostTest.java
+++ horbachovProj/src/test/java/postsTest/CreatePostTest.java
@@ -1,25 +1,29 @@
 package postsTest;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreatePostTest extends BaseTest {
+    final String title = "G4-horbachov" + Util.getDateAndTimeFormatted();
     @Test
     public void createNewPost(){
+
         loginPage
                 .loginWithValidCred()
                 .checkIsbuttonSignOutDisplayed()
                 .clickOnCreatePostButton();
         createPostPage
                 .checkIsRedirectToCreatePostPage()
-                .enterTextIntoTitleInput("G4-horbachov")
+                .enterTextIntoTitleInput(title)
                 .enterTextIntoBodyInput("Body post")
  //               .selectTextInDropDownRole("Частное сообщение")
                 .selectValueInDropDownRole("One Person")
                 .clickOnButtonSaveNewPost()
                 .checkIsRedirectToPostPage()
                 .checkTextInAlert("New post successfully created.")
-        ;
+                .clickOnMyProfileButton()
+                .checkPostWasCreated(title);
 
 
     }
