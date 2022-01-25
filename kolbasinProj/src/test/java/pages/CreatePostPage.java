8f4d1f013cdf24f599bee51b879f343bf14a64b8
Kolbasin1 <andrejkolbas@gmail.com>
2022-01-25 21:40:32 +0200

    add new code to my study project(url)

--- kolbasinProj/src/test/java/pages/CreatePostPage.java
+++ kolbasinProj/src/test/java/pages/CreatePostPage.java
@@ -22,8 +22,14 @@ public class CreatePostPage extends ParentPageWithHeaders{
         super(webDriver);
     }
 
+    @Override
+    String getRelativeUrl() {
+        return "/create-post";
+    }
+
     public CreatePostPage checkIsRedirectToCreatePostPage(){
         waitChatTobeHide();
+        checkUrl();
         Assert.assertTrue("InputTitle is not displayed"
                 , isElementDisplayed(inputTitle));
         return this;
