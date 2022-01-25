1a9d4af62721573c71f8a7079d7f90da9b38f898
Roman <roman@defentry.com>
2022-01-25 21:42:08 +0200

    UPD

--- siforovProj/src/main/java/pages/HomePage.java
+++ siforovProj/src/main/java/pages/HomePage.java
@@ -1,7 +1,5 @@
 package pages;
 
-import jdk.internal.net.http.common.Log;
-import libs.TestData;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
