7a7410fcf3433b9019c00ab8392cbe75aa5a732a
Anna-Semenova <anushiksemenova75@gmail.com>
2022-01-27 21:40:47 +0200

    registration test

--- semenovaProj/src/test/java/registrationTests/RegistrationTestWithPageObject.java
+++ semenovaProj/src/test/java/registrationTests/RegistrationTestWithPageObject.java
@@ -4,16 +4,32 @@ import baseTest.BaseTest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class RegistrationTestWithPageObject extends BaseTest {
     @Test
     public void unValidRegistration() {
         loginPage.openLoginPage();
-        loginPage.enterUsernameIntoInputPickUsername("tr");
-        loginPage.enterEmailIntoInputEmail("test.com");
-        loginPage.enterCreatePasswordIntoInputCreatePaswword("123");
+        loginPage.enterLoginRegistration("tr");
+        loginPage.enterEmailRegistration("test.com");
+        loginPage.enterCreatePasswordRegistration("123");
         loginPage.clickOnButtonSignUpForOurApp();
         loginPage.checkIsMessagesDisplayed();
         Assert.assertFalse("Go to page HomePage", homePage.isButtonSignOutDisplayed());
+
+    }
+    String expectedErrors = "Username must be at least 3 characters.;" +
+            "You must provide a valid email address.;" +
+            "Password must be at least 12 characters.";
+    @Test
+    public void registrationErrors(){
+        loginPage.openLoginPage();
+        loginPage.enterLoginRegistration("tr")
+                .enterEmailRegistration("qqq")
+                .enterCreatePasswordRegistration("234")
+                .checkErrorMessages(expectedErrors);
+
+
 
     }
 }
