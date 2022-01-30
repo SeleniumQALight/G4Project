c4254ad464a9e6931bdc00caa5bd9bab0a3af3ee
Ivan <ivan.taurus777@gmail.com>
2022-01-30 16:41:15 +0200

    Edit post test HW6

---
+++ petkoProj/src/main/java/pages/EditPostPage.java
@@ -0,0 +1,55 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditPostPage extends ParentPageWithHeader{
+
+    @FindBy(xpath = ".//button[text()='Save Updates']")
+    private WebElement buttonSaveUpdates;
+
+    @FindBy(name = "title")
+    private WebElement inputTitle;
+
+    @FindBy(xpath = ".//*[@class='alert alert-success text-center']")
+    private WebElement alertSuccess;
+
+    @FindBy(xpath = ".//a[text()='« Back to post permalink']")
+    private WebElement linkToPostPage;
+
+    public EditPostPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    @Override
+    String getRelativeUrl() {
+        return "/edit";
+    }
+
+    public EditPostPage checkIsRedirectToEditPostPage(){
+        waitChatToBeHide();
+//        checkUrlWithPattern();
+        Assert.assertTrue("Button 'Save updates' is not displayed", isElementDisplayed(buttonSaveUpdates));
+        return this;
+    }
+
+    public EditPostPage checkTextInAlert(String text){
+        Assert.assertEquals("Text in Alert",text,alertSuccess.getText());
+        logger.info("Successfully edit message in the alert is present");
+        return this;
+    }
+
+    public EditPostPage editTextInTitleInputAndSaveChanges(String newTitle){
+        inputTitle.clear();
+        enterTextInToElement(inputTitle, newTitle);
+        clickOnElement(buttonSaveUpdates);
+        return this;
+    }
+
+    public PostPage clickOnLinkToPostPage(){
+        clickOnElement(linkToPostPage);
+        return new PostPage(webDriver);
+    }
+}
---
+++ petkoProj/src/test/java/postsTests/EditPostTest.java
@@ -0,0 +1,51 @@
+package postsTests;
+
+import baseTest.BaseTest;
+import libs.Util;
+import org.junit.After;
+import org.junit.Test;
+
+public class EditPostTest extends BaseTest {
+
+    final String title = "G4-Petko" + Util.getDateAndTimeFormatted();
+    final String addTitle = "Edited";
+
+    @Test
+    public void EditPostTitle(){
+        loginPage
+                .loginWithValidCred()
+                .checkIsButtonSignOutDisplayed()
+                .clickOnCreatePostButton()
+                .checkIsRedirectToCreatePostPage()
+                .enterTextIntoTitleInput(title)
+                .enterTextIntoBodyInput("Body post")
+                .selectValueInDropDownRole("One Person")
+                .clickOnButtonSaveNewPost()
+                .checkIsRedirectToPostPage()
+                .checkTextInAlert("New post successfully created.")
+                .clickOnMyProfileButton()
+                .checkPostWasCreated(title)
+                .clickOnPostName(title)
+                .clickOnEditButton()
+                .checkIsRedirectToEditPostPage()
+                .editTextInTitleInputAndSaveChanges(title + addTitle)
+                .checkTextInAlert("Post successfully updated.")
+                .clickOnLinkToPostPage()
+                .checkIsRedirectToPostPage()
+                .checkTextInTitle(title + addTitle);
+
+
+    }
+
+    @After
+    public void deletePost(){
+        homePage
+                .openHomePage()
+                .checkIsButtonSignOutDisplayed()
+                .clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .deletePostWithTitleWhilePresent(title + addTitle);
+    }
+
+
+}
