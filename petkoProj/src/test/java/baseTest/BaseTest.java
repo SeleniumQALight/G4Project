ca9fa00a244e5b7a273d71cde0e5eabac71165c9
Ivan <ivan.taurus777@gmail.com>
2022-02-01 21:05:56 +0200

    browsers

--- petkoProj/src/test/java/baseTest/BaseTest.java
+++ petkoProj/src/test/java/baseTest/BaseTest.java
@@ -8,6 +8,9 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import pages.CreatePostPage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -26,8 +29,7 @@ public class BaseTest {
     @Before
     public void setUp(){
         logger.info("------- " + testName.getMethodName() + " was started -------" );
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+        webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
         logger.info("Browser was opened");
@@ -45,5 +47,25 @@ public class BaseTest {
 
     @Rule
     public TestName testName = new TestName();
+
+    private WebDriver initDriver(){
+        String browser = System.getProperty("browser");
+        if((browser==null) || browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        }else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            webDriver = new EdgeDriver();
+        }else if ("ie".equalsIgnoreCase(browser)) {
+            //WebDriverManager.iedriver().setup();
+            // in most cases 32bit version is needed
+            WebDriverManager.iedriver().arch32().setup();
+            return new InternetExplorerDriver();
+        }
+        return webDriver;
+    }
 
 }
