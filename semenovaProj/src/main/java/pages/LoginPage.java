e42946e861431225589922cb3a358e29857708d3
Anna-Semenova <anushiksemenova75@gmail.com>
2022-02-01 20:06:26 +0200

    parametris

--- semenovaProj/src/main/java/pages/LoginPage.java
+++ semenovaProj/src/main/java/pages/LoginPage.java
@@ -157,6 +157,7 @@ public class LoginPage extends ParentPage {
                 .withMessage("Numbers of messages ")
                 .until(ExpectedConditions.numberOfElementsToBe(
                         By.xpath(listErrorsLocator), expectedErrorsArray.length));
+        Assert.assertEquals("", expectedErrorsArray.length, listOfErrors.size());
         ArrayList<String> actualTextFromErrors = new ArrayList<>();
         for (WebElement element: listOfErrors){
             actualTextFromErrors.add(element.getText());
