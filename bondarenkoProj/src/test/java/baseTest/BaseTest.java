5e91fd13185ea86665c8376ebcd7ff6960f3d835
AnBondarenko <bondarenko.nastya86@gmail.com>
2022-02-01 21:05:51 +0200

    browsers

--- bondarenkoProj/src/test/java/baseTest/BaseTest.java
+++ bondarenkoProj/src/test/java/baseTest/BaseTest.java
@@ -8,6 +8,8 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import pages.CreatePostPage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -23,8 +25,7 @@ public class BaseTest {
     @Before
     public void setUp(){
         logger.info("----" + testName.getMethodName() + " was started -------");
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+        webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
         logger.info("Browser was opened");
@@ -43,4 +44,21 @@ public class BaseTest {
 
     @Rule
     public  TestName testName = new TestName();
+
+    private WebDriver initDriver(){
+        String browser = System.getProperty("browser");
+        if ((browser==null) || browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            webDriver =  new ChromeDriver();
+        }else if (browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        }else if ("ie".equalsIgnoreCase(browser)) {
+            //WebDriverManager.iedriver().setup();
+            // in most cases 32bit version is needed
+            WebDriverManager.iedriver().arch32().setup();
+            return new InternetExplorerDriver();
+}
+        return webDriver;
+    }
 }
