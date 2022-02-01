2b0f4358b690991a81d7b29656cf958ebce88526
Latsynskyi <latsynskyi@gmail.com>
2022-02-01 21:06:27 +0200

    Drivers for firefox and ie

--- latsynskyiProj/src/test/java/baseTest/BaseTest.java
+++ latsynskyiProj/src/test/java/baseTest/BaseTest.java
@@ -8,6 +8,8 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import pages.CreatePostPage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -23,9 +25,10 @@ public class BaseTest {
    protected CreatePostPage createPostPage;
     @Before
     public void setUp(){
-        logger.info("------"+testName.getMethodName()+"     was started ----------");
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+        logger.info("------    "+testName.getMethodName()+"     was started ----------");
+        webDriver = initDriver();
+//        WebDriverManager.chromedriver().setup();
+//        webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
         logger.info("Browser was opened");
@@ -37,12 +40,29 @@ public class BaseTest {
     public void tearDown(){
         webDriver.quit();
         logger.info("Browser was closed");
-        logger.info("------"+testName.getMethodName()+"    was ended ----------\n");// \n
-
+        logger.info("------     "+testName.getMethodName()+"    was ended ----------\n");// \n
         logger.info("");
         logger.info("");
     }
 @Rule
     public TestName testName = new TestName();
 
+
+    private WebDriver initDriver(){
+      String browser = System.getProperty("browser");
+    if ((browser==null)|| browser.equalsIgnoreCase("chrome")){
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }else if (browser.equalsIgnoreCase("firefox")){
+        WebDriverManager.firefoxdriver().setup();
+        webDriver = new FirefoxDriver();
+    }else  if ("ie".equalsIgnoreCase(browser)){
+//        WebDriverManager.iedriver().setup();
+//        in most cases 32bit version is needed
+                WebDriverManager.iedriver().arch32().setup();
+       return  new InternetExplorerDriver();
+    }
+
+    return webDriver;
+    }
 }
