0e27e07eadc07b45d33a47a1c816d9ae2858659f
viktor.loboda <valksua@gmail.com>
2022-02-01 20:15:29 +0200

    rule

--- lobodaProj/src/test/java/registrationTest/RegistrTestWithPageObject.java
+++ lobodaProj/src/test/java/registrationTest/RegistrTestWithPageObject.java
@@ -14,8 +14,8 @@ public class RegistrTestWithPageObject extends BaseTest {
     @Test
     @Parameters({
             "tr,qqq,345,Username must be at least 3 characters.;You must provide a valid email address.;Password must be at least 12 characters."
-            , "tr,test@qqqq.com,123456qwert,Username must be at least 3 characters."
-            , "tr,qqq,123456qwert,Username must be at least 3 characters.;You must provide a valid email address."
+            , "tr,test@qqqq.com,123456qwerty,Username must be at least 3 characters."
+            , "tr,qqq,123456qwerty,Username must be at least 3 characters.;You must provide a valid email address."
     })
     @TestCaseName("registrationErrors : login = {0}, email = {1}, password = {2}")
     public void registrationErrors(String login, String email, String passWord, String expectedErrors){
