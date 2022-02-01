4e2e80f14ec23e2a5fa02385517f547baa564f29
viktor.loboda <valksua@gmail.com>
2022-02-01 20:08:01 +0200

    parameterized test

--- lobodaProj/src/test/java/registrationTest/RegistrTestWithPageObject.java
+++ lobodaProj/src/test/java/registrationTest/RegistrTestWithPageObject.java
@@ -1,18 +1,29 @@
 package registrationTest;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class RegistrTestWithPageObject extends BaseTest {
-    String expectedErrors = "Username must be at least 3 characters.;You must provide a valid email address.;Password must be at least 12 characters.";
+//    String expectedErrors = "Username must be at least 3 characters.;You must provide a valid email address.;Password must be at least 12 characters.";
 
     @Test
-    public void registrationErrors(){
+    @Parameters({
+            "tr,qqq,345,Username must be at least 3 characters.;You must provide a valid email address.;Password must be at least 12 characters."
+            , "tr,test@qqqq.com,123456qwert,Username must be at least 3 characters."
+            , "tr,qqq,123456qwert,Username must be at least 3 characters.;You must provide a valid email address."
+    })
+    @TestCaseName("registrationErrors : login = {0}, email = {1}, password = {2}")
+    public void registrationErrors(String login, String email, String passWord, String expectedErrors){
         loginPage.openLoginPage();
         loginPage
-                .enterLoginRegistration("tr")
-                .enterEmailRegistration("qqq")
-                .enterPassWordRegistration("345")
+                .enterLoginRegistration(login)
+                .enterEmailRegistration(email)
+                .enterPassWordRegistration(passWord)
                 .checkErrorsMesseges(expectedErrors);
     }
 }
