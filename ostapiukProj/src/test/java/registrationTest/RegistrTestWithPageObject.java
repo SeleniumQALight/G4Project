4161e563fe4f1e4f35aad1fed60bc164d9f1dc60
AndriiOstapiuk <andrii.ostapiuk@gmail.com>
2022-02-04 15:17:30 +0200

    Ostapiuk 01 27 registration test

---
+++ ostapiukProj/src/test/java/registrationTest/RegistrTestWithPageObject.java
@@ -0,0 +1,19 @@
+package registrationTest;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class RegistrTestWithPageObject extends BaseTest {
+    String expectedErrors = "Username must be at least 3 characters.;You must provide a valid email address.;Password must be at least 12 characters.";
+
+    @Test
+    public void registrationErrors(){
+        loginPage.openLoginPage();
+        loginPage
+                .enterLoginRegistration("tr")
+                .enterEmailRegistration("qqq")
+                .enterPassWordRegistration("asd")
+                .checkErrorsMessages(expectedErrors)
+        ;
+    }
+}
