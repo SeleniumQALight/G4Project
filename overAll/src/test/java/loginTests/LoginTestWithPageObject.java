e5942f8f066d1edb65150a6071dcd7c588a60a8c
Taras Radulenko <Taras_Radulenko@epam.com>
2022-02-10 21:28:25 +0200

    Ignore

--- overAll/src/test/java/loginTests/LoginTestWithPageObject.java
+++ overAll/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -26,6 +27,7 @@ public class LoginTestWithPageObject extends BaseTest {
                 , homePage.isButtonSignOutDisplayed());
     }
     @Test
+    @Ignore
     public void validLoginTestWithExcel() throws IOException {
         Map<String,String> dataForValidLogin = ExcelDriver.getData(configProperties.DATA_FILE(),"validLogOn");
         loginPage.openLoginPage();
