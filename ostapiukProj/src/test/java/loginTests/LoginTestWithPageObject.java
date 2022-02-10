8b1195d64e6aef15683f92e34ec5e4c9ce66958e
AndriiOstapiuk <andrii.ostapiuk@gmail.com>
2022-02-10 20:39:27 +0200

    Ostapiuk 02 10 suits

--- ostapiukProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ ostapiukProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,9 +1,11 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import categories.SmokeTestFilter;
 import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 //import pages.ParentPage;
 
 import java.io.IOException;
@@ -13,6 +15,7 @@ import static pages.ParentPage.configProperties;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
+    @Category(SmokeTestFilter.class)
     public void validLoginTest(){
         loginPage.openLoginPage();
         loginPage.enterLoginIntoInputLogin("qaauto");
