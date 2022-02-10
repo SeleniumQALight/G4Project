372b3870fcfa0f9d8ae2bdc24dca408ffa1fc314
viktor.loboda <valksua@gmail.com>
2022-02-10 21:30:32 +0200

    Ignore

--- lobodaProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ lobodaProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -4,6 +4,7 @@ import baseTest.BaseTest;
 import categories.SmokeTestFilter;
 import libs.ExcelDriver;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -27,6 +28,7 @@ public class LoginTestWithPageObject extends BaseTest {
     }
 
     @Test
+    @Ignore
     public void validLoginTestWithExcel() throws IOException {
         Map<String,String> dataForValidLogin = ExcelDriver.getData(configProperties.DATA_FILE(), "validLogOn");
         loginPage.openLoginPage();
