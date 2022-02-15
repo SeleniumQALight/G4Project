8c8f06ff0cf7462f09853b42dd3ee393a7980caa
Kolbasin1 <andrejkolbas@gmail.com>
2022-02-15 21:37:54 +0200

    report

--- kolbasinProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ kolbasinProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -1,18 +1,28 @@
 package loginTest;
 
 import baseTest.BaseTest;
+import io.qameta.allure.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+@Epic("Allure examples")
+@Feature("Junit 4 support")
 public class LoginTestWithPageObject extends BaseTest {
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
     @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Base support for bdd annotations")
     public void validLoginTest(){
     loginPage.openLoginPage();
     loginPage.enterLoginIntoInputLogin("qaauto");
     loginPage.enterPassWordIntoInputPassWord("123456qwerty");
     loginPage.clickOnButtonSignIn();
 
-    Assert.assertTrue("Button SingOut is not displayed"  // отработает только тогда, когда появится кнопка
+    checkER("Button SingOut is not displayed"  // отработает только тогда, когда появится кнопка
             , homePage.isButtonSignOutDisplayed());
     }
 }
