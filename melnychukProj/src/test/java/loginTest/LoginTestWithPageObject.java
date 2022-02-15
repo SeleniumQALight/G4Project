3569310c538683792fffd6c3e2f2d41862a8aba9
omelaT <olha.melnichuk@gmail.com>
2022-02-15 21:29:54 +0200

    allure

--- melnychukProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ melnychukProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -3,18 +3,29 @@ package loginTest;
 
 import baseTest.BaseTest;
 import categories.SmokeTestFilter;
+import io.qameta.allure.*;
 import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import pages.ParentPage;
+
 
 import java.io.IOException;
 import java.util.Map;
 
 import static pages.ParentPage.configProperties;
+@Epic("Allure examples")
+@Feature("Junit 4 support")
+
 
 public class LoginTestWithPageObject extends BaseTest {
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Base support for bdd annotations")
     @Test
     @Category(SmokeTestFilter.class)
     public void validLoginTest(){
@@ -23,7 +34,7 @@ public class LoginTestWithPageObject extends BaseTest {
         loginPage.enterPasswordIntoInputPassword("123456qwerty");
         loginPage.clickOnButtonSignIn();
 
-        Assert.assertTrue("Button signOut is displayed ", homePage.isButtonSignOutDisplayed());
+    checkExpectedResult("Button signOut is displayed ", homePage.isButtonSignOutDisplayed());
     }
     @Test
     public void validLoginTestWithExcel() throws IOException {
