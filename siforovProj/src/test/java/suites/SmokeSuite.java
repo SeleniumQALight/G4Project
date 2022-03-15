d80b46256aa240d5e96f22e7cca3619e46b80c55
Roman <roman@defentry.com>
2022-03-15 20:52:34 +0200

    Suites

---
+++ siforovProj/src/test/java/suites/SmokeSuite.java
@@ -0,0 +1,17 @@
+package suites;
+
+import LoginTest.LoginTestWithPageObject;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import pages.CreatePostPage;
+import postsTests.CreatePostTest;
+import registrationTests.RegistrationTestsSecondVersion;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        LoginTestWithPageObject.class,
+        RegistrationTestsSecondVersion.class,
+        CreatePostTest.class
+})
+public class SmokeSuite {
+}
