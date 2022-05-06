0bb9a289cfa1be6e259198925c8a668d68cb8ac3
Roman <roman@defentry.com>
2022-05-06 10:49:36 +0300

    RestAssured response parsing

--- siforovProj/src/test/java/apiTests/ApiTests.java
+++ siforovProj/src/test/java/apiTests/ApiTests.java
@@ -2,6 +2,7 @@ package apiTests;
 
 import api.Endpoints;
 import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,17 +10,26 @@ import static io.restassured.RestAssured.given;
 public class ApiTests {
 
     final String USER_NAME = "autoapi";
+    Logger logger = Logger.getLogger(getClass());
 
     @Test
     public void getAllPostsByUser() {
-        given()
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .log().all()
-        .when()
-                .get(Endpoints.POST_BY_USER, USER_NAME)
-        .then()
-                .statusCode(200)
-                .log().all();
+        PostDTO[] responseBody =
+                given()
+                        .contentType(ContentType.JSON)
+                        .accept(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(Endpoints.POST_BY_USER, USER_NAME)
+                        .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract()
+                        .response()
+                        .as(PostDTO[].class);
+
+        logger.info("Number of messages is " + responseBody.length);
+        logger.info("Post name is " + responseBody[0].title);
+        logger.info("Username " + responseBody[0].author.username);
     }
 }
---
+++ siforovProj/src/test/java/apiTests/AuthorDTO.java
@@ -0,0 +1,30 @@
+package apiTests;
+
+public class AuthorDTO {
+    String username;
+    String avatar;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorDTO{" +
+                "username='" + username + '\'' +
+                ", avatar='" + avatar + '\'' +
+                '}';
+    }
+}
