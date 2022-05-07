f9ed09cd236b09f7925c2b2f8cfab101604259cf
Livecounter86 <livecounter@i.ua>
2022-05-07 12:38:31 +0300

    Check of API exchange rates

--- oliinykProj/src/main/java/api/EndPoints.java
+++ oliinykProj/src/main/java/api/EndPoints.java
@@ -3,4 +3,5 @@ package api;
 public interface EndPoints {
     String baseUrl = "http://qa-complex-app-for-testing.herokuapp.com";
     String POST_BY_USER = baseUrl + "/api/postsByAuthor/{0}";
+    String exchangeURL = "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11";
 }
