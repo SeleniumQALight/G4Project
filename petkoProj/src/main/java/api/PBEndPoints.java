f08ec04070b262fbee99bc13043649fcee8eaf56
Ivan <ivan.taurus777@gmail.com>
2022-05-10 19:20:47 +0300

    HW1 A few fixes

--- petkoProj/src/main/java/api/PBEndPoints.java
+++
@@ -1,8 +0,0 @@
-package api;
-
-public interface PBEndPoints {
-
-    String baseUrl = "https://api.privatbank.ua";
-    String EXCHANGE_RATE = baseUrl + "/p24api/pubinfo?exchange&json&coursid=11";
-
-}
---
+++ petkoProj/src/main/java/api/privatBank/PBEndPoints.java
@@ -0,0 +1,8 @@
+package api.privatBank;
+
+public interface PBEndPoints {
+
+    String baseUrl = "https://api.privatbank.ua";
+    String EXCHANGE_RATE = baseUrl + "/p24api/pubinfo";
+
+}
