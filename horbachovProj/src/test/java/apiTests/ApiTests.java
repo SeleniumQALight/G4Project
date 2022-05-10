958c4c827312eb4ab7974b53b9f6680ae9dd6439
loopchoker <fastmotionsg@gmail.com>
2022-05-10 21:13:58 +0300

    api tests: schema, string, lombok

--- horbachovProj/src/test/java/apiTests/ApiTests.java
+++ horbachovProj/src/test/java/apiTests/ApiTests.java
@@ -4,12 +4,17 @@ import api.AuthorDTO;
 import api.EndPoints;
 import api.PostDTO;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class ApiTests {
     final String USER_NAME = "autoapi";
@@ -36,8 +41,15 @@ public class ApiTests {
         }
 
         PostDTO[] expectedPostDTO = {
-                new PostDTO("test", "test body", "All Users", new AuthorDTO("autoapi"), false),
-                new PostDTO("test2", "test body2", "All Users", new AuthorDTO("autoapi"), false)
+//                new PostDTO("test", "test body", "All Users", new AuthorDTO("autoapi"), false),
+//                new PostDTO("test2", "test body2", "All Users", new AuthorDTO("autoapi"), false)
+
+                PostDTO.builder().title("test").body("test body").select1("All Users")
+                        .author(AuthorDTO.builder().username("autoapi").build()).isVisitorOwner(false)
+                        .build(),
+                PostDTO.builder().title("test2").body("test body2").select1("All Users")
+                        .author(AuthorDTO.builder().username("autoapi").build()).isVisitorOwner(false)
+                        .build()
         };
 
         Assert.assertEquals(expectedPostDTO.length, responseBody.length);
@@ -53,5 +65,69 @@ public class ApiTests {
         }
 
         softAssertions.assertAll();
+    }
+
+    @Test
+    public void getAllPostsByUserNegative(){
+        String actualResponse =
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                 .when()
+                        .get(EndPoints.POST_BY_USER, "notValidUser")
+                .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response().getBody().asString();
+
+        Assert.assertEquals("Message in response ", "\"Sorry, invalid user requested.undefined\"", actualResponse);
+        Assert.assertEquals("Message in response ", "Sorry, invalid user requested.undefined", actualResponse.replace("\"",""));
+
+
+
+    }
+
+    @Test
+    public void getAllPostsByUserPath(){
+        Response response =
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                .when()
+                        .get(EndPoints.POST_BY_USER, USER_NAME)
+                .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response();
+
+        List<String> actualTitleList = response.jsonPath().getList("title", String.class);
+        List<Map> actualAuthorList = response.jsonPath().getList("author", Map.class);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        for (int i = 0; i < actualAuthorList.size(); i++) {
+            softAssertions.assertThat(actualTitleList.get(i)).as("Item number " + i).contains("test");
+        }
+        for (int i = 0; i < actualAuthorList.size(); i++) {
+            softAssertions.assertThat(actualAuthorList.get(i).get("username"))
+                    .as("Item number " + i).isEqualTo(USER_NAME);
+
+        }
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    public void getAllPostsByUserSchema() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+        .when()
+                .get(EndPoints.POST_BY_USER, USER_NAME)
+        .then()
+                .statusCode(200)
+                .log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("respons.json"));
+
     }
 }
