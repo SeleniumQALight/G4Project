4ab9cc9761a40e582ef261d9a27ad4e1522eb544
Roman <roman@defentry.com>
2022-05-12 21:28:02 +0300

    API test with posts deletion

--- siforovProj/src/main/java/api/Endpoints.java
+++ siforovProj/src/main/java/api/Endpoints.java
@@ -3,4 +3,7 @@ package api;
 public interface Endpoints {
     String baseUrl = "http://qa-complex-app-for-testing.herokuapp.com";
     String POST_BY_USER = baseUrl + "/api/postsByAuthor/{0}";
+    String LOGIN = baseUrl + "/api/login";
+    String CREATE_POST = baseUrl + "/api/create-post";
+    String DELETE_POST = baseUrl + "/api/post/{0}";
 }
