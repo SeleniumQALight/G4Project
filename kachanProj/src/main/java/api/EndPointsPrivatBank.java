38cff63573ae529251d9fb893ff9519dab65996f
Yulia Kachan <yuleyka@ukr.net>
2022-05-16 00:24:41 +0300

    HW_1

---
+++ kachanProj/src/main/java/api/EndPointsPrivatBank.java
@@ -0,0 +1,6 @@
+package api;
+
+public interface EndPointsPrivatBank {
+    String baseUrlPrivat = "https://api.privatbank.ua";
+    String CURRENCY_EXCHANGE = baseUrlPrivat + "/p24api/pubinfo?exchange&json&coursid=11";
+}
