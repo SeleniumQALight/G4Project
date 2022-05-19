2c3fd87b48a0606d3cb41484712ed036e5cd46fc
Yulia Kachan <yuleyka@ukr.net>
2022-05-19 21:22:40 +0300

    cucumber test

--- kachanProj/src/test/java/stepDefinitions/Hooks.java
+++ kachanProj/src/test/java/stepDefinitions/Hooks.java
@@ -1,5 +1,19 @@
-package stepDefinitions;
+package StepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import libs.DriverHelper;
 
 public class Hooks {
-    //TODO before should be created
+    DriverHelper driverHelper = new DriverHelper();
+
+    @Before
+    public void setUp() {
+        driverHelper.createDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driverHelper.closeDriver();
+    }
 }
