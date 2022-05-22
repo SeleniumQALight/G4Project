705c82be7bda258e032749fa6251b0b3269bb472
loopchoker <fastmotionsg@gmail.com>
2022-05-22 19:27:27 +0300

    error messages

--- horbachovProj/src/test/java/stepDefinitions/LoginPage_StepDefinition.java
+++ horbachovProj/src/test/java/stepDefinitions/LoginPage_StepDefinition.java
@@ -16,35 +16,76 @@ public class LoginPage_StepDefinition {
         loginPage.openLoginPage();
     }
 
-        @When("^User enters '(.*)' login into 'Login' input on 'Login' page$")
-        public void user_enters_Wrong_login_login_into_Login_input_on_Login_page(String username)  {
+    @When("^User enters '(.*)' login into 'Login' input on 'Login' page$")
+    public void user_enters_Wrong_login_login_into_Login_input_on_Login_page(String username) {
         loginPage.enterLoginIntoInputLogin(username);
 
-        }
+    }
 
-        @When("^User enters '(.*)' passWord into 'PassWord' input on 'Login' page$")
-        public void user_enters_passWord_passWord_into_PassWord_input_on_Login_page(String password)  {
+    @When("^User enters '(.*)' passWord into 'PassWord' input on 'Login' page$")
+    public void user_enters_passWord_passWord_into_PassWord_input_on_Login_page(String password) {
         loginPage.enterPasswordIntoInputPassword(password);
 
-        }
+    }
 
-        @When("^User click on 'SingIn' button on 'Login' page$")
-        public void user_click_on_SingIn_button_on_Login_page() {
+    @When("^User click on 'SingIn' button on 'Login' page$")
+    public void user_click_on_SingIn_button_on_Login_page() {
         loginPage.clickOnButtonSignIn();
-        }
+    }
 
-        @Then("^User sees alert message with text '(.*)'$")
-        public void user_sees_alert_message_with_text_Invalid_username_password(String message) {
+    @Then("^User sees alert message with text '(.*)'$")
+    public void user_sees_alert_message_with_text_Invalid_username_password(String message) {
         loginPage.checkAlertMessageText(message);
-            }
+    }
 
-        @Then("^User sees 'Sign Out' button$")
-        public void user_sees_sign_out_button(){
+    @Then("^User sees 'Sign Out' button$")
+    public void user_sees_sign_out_button() {
         homePage.isButtonSignOutDisplayed();
 
-        }
+    }
+
+    @When("^User enters '(.*)' into 'Username' input on 'Login' page$")
+    public void user_enters_username_into_input_on_login_page_registration(String username) {
+        loginPage.enterLoginRegistration(username);
 
     }
+
+    @When("^User enters '(.*)' into 'Email' input on 'Login' page$")
+    public void user_enters_email_into_input_on_login_page_registration(String email) {
+        loginPage.enterEmailRegistration(email);
+
+    }
+
+    @When("^User enters '(.*)' into 'Password' input on 'Login' page$")
+    public void user_enters_password_into_input_on_login_page_registration(String password) {
+        loginPage.enterPasswordRegistration(password);
+
+    }
+
+    @When("^User click on 'Sign Up for OurApp' button on 'Login' page$")
+    public void user_click_on_SingUp_button_on_Login_page() {
+        loginPage.clickOnButtonSignUp();
+
+    }
+
+    @Then("^User sees 'That username is already taken.' error message")
+    public void user_sees_that_username_is_already_taken_error_message() {
+        loginPage.checkErrorsMessages("That username is already taken.");
+
+    }
+
+    @Then("^User sees 'You must provide a valid email address.' error message")
+    public void user_sees_you_must_provide_valid_email_error_message() {
+        loginPage.checkErrorsMessages("You must provide a valid email address.");
+
+    }
+
+    @Then("^User sees 'Password must be at least 12 characters.' error message")
+    public void user_sees_password_must_be_at_least_12_characters_error_message() {
+        loginPage.checkErrorsMessages("Password must be at least 12 characters.");
+
+    }
+}
 
 
 
--- horbachovProj/src/test/resources/loginTests/login.feature
+++ horbachovProj/src/test/resources/loginTests/login.feature
@@ -14,7 +14,7 @@ Feature: User Login
       | Wrong login | 1234     |
 
   @R002
-  Scenario Outline: R001 Login with valid Login
+  Scenario Outline: R002 Login with valid Login
     Given User opens 'Login' page
     When User enters '<login>' login into 'Login' input on 'Login' page
     And User enters '<password>' passWord into 'PassWord' input on 'Login' page
@@ -24,3 +24,44 @@ Feature: User Login
     Examples:
       | login       | password |
       | serge | qwerty123456     |
+
+
+  @R003
+  Scenario Outline: R003 Registration with existing username
+    Given User opens 'Login' page
+    When User enters '<username>' into 'Username' input on 'Login' page
+    And User enters '<email>' into 'Email' input on 'Login' page
+    And User enters '<password>' into 'Password' input on 'Login' page
+    And User click on 'Sign Up for OurApp' button on 'Login' page
+    Then User sees 'That username is already taken.' error message
+
+    Examples:
+      | username       | email | password |
+      | serge | testemail@proton.com    | 123eqwwewrewrewrw |
+
+  @R004
+  Scenario Outline: R004 Registration with invalid email
+    Given User opens 'Login' page
+    When User enters '<username>' into 'Username' input on 'Login' page
+    And User enters '<email>' into 'Email' input on 'Login' page
+    And User enters '<password>' into 'Password' input on 'Login' page
+    And User click on 'Sign Up for OurApp' button on 'Login' page
+    Then User sees 'You must provide a valid email address.' error message
+
+    Examples:
+      | username       | email | password |
+      | serge1 | testemail   | 123eqwwewrewrewrw |
+
+
+  @R005
+  Scenario Outline: R005 Registration with invalid password
+    Given User opens 'Login' page
+    When User enters '<username>' into 'Username' input on 'Login' page
+    And User enters '<email>' into 'Email' input on 'Login' page
+    And User enters '<password>' into 'Password' input on 'Login' page
+    And User click on 'Sign Up for OurApp' button on 'Login' page
+    Then User sees 'Password must be at least 12 characters.' error message
+
+    Examples:
+      | username       | email | password |
+      | serge1 | testemail@boom.com   | 123 |
