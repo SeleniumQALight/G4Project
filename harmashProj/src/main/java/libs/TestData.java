d1690dd0400b2c9e49c97cbecd7db474cd0280e0
Faina Harmash <faya.garmash.work@gmail.com>
2022-05-24 21:46:46 +0300

    added cucumber test 2

--- harmashProj/src/main/java/libs/TestData.java
+++ harmashProj/src/main/java/libs/TestData.java
@@ -1,6 +1,6 @@
 package libs;
 
 public class TestData {
-    public final static String VALID_LOGIN = "qaauto";
-    public final static String VALID_PASS = "123456qwerty";
+    public final static String VALID_LOGIN = "fai";
+    public final static String VALID_PASS = "fai123456789";
 }
--- harmashProj/src/test/java/StepDefinitions/Hooks.java
+++ harmashProj/src/test/java/StepDefinitions/Hooks.java
@@ -1,20 +1,36 @@
 package StepDefinitions;
 
 
+import api.ApiHelper;
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import libs.DriverHelper;
+import libs.TestData;
+import org.apache.log4j.Logger;
 
 public class Hooks {
-DriverHelper driverHelper = new DriverHelper();
-   @Before
-    public void setUp(){
-       driverHelper.createDriver();
+    DriverHelper driverHelper = new DriverHelper();
+    private ApiHelper apiHelper = new ApiHelper();
+    Logger logger = Logger.getLogger(getClass());
 
-   }
-   @After
-    public void tearDown(){
-       driverHelper.closeDriver();
+    @Before(order = 0)
+    public void setUp(Scenario scenario) {
+        logger.info(scenario.getName() + " was started");
+        driverHelper.createDriver();
+    }
 
-   }
+    @After(order = 0)
+    public void tearDown(Scenario scenario) {
+        driverHelper.closeDriver();
+        logger.info(scenario.getName() + " was ended with status " + scenario.getStatus());
+    }
+
+    @Before(value = "@BeforeDeletingAllPostsForDefaultUser", order = 100)
+    @After(value = "@AfterDeletingAllPostsForDefaultUser", order = 100)
+    public void deleteAllPostsForDefaultUser() {
+        apiHelper.deletePostsTillPresent(TestData.VALID_LOGIN, TestData.VALID_PASS);
+
+    }
+
 }
