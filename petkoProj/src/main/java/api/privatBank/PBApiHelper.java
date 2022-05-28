609a61aee7c17b54628f4671e373203c7c091caf
Ivan <ivan.taurus777@gmail.com>
2022-05-28 13:36:44 +0300

    API part1

---
+++ petkoProj/src/main/java/api/privatBank/PBApiHelper.java
@@ -0,0 +1,41 @@
+package api.privatBank;
+
+import io.restassured.http.ContentType;
+import libs.PBTestData;
+
+
+import java.util.Objects;
+
+import static io.restassured.RestAssured.given;
+
+public class PBApiHelper {
+
+    public void getCurrencyRate() {
+        PBExchangeRateDTO[] responseBody = given()
+                .contentType(ContentType.JSON)
+                .param("exchange")
+                .param("json")
+                .param("coursid", 5)
+                .log().all()
+                .when()
+                .get(PBEndPoints.EXCHANGE_RATE)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract().response().as(PBExchangeRateDTO[].class);
+
+        for (int i = 0; i < responseBody.length; i++) {
+
+            if(Objects.equals(responseBody[i].getCcy(), "USD")){
+                PBTestData.API_USD_BUY_RATE = responseBody[i].getBuy().substring(0,5);
+                PBTestData.API_USD_SALE_RATE = responseBody[i].getSale().substring(0,5);
+            }
+
+            if(Objects.equals(responseBody[i].getCcy(), "EUR")){
+                PBTestData.API_EUR_BUY_RATE = responseBody[i].getBuy().substring(0,5);
+                PBTestData.API_EUR_SALE_RATE = responseBody[i].getSale().substring(0,5);
+            }
+
+        }
+    }
+}
---
+++ petkoProj/src/main/java/libs/PBTestData.java
@@ -0,0 +1,14 @@
+package libs;
+
+public class PBTestData {
+
+    public static String API_USD_BUY_RATE;
+    public static String API_USD_SALE_RATE;
+    public static String API_EUR_BUY_RATE;
+    public static String API_EUR_SALE_RATE;
+
+    public static String UI_USD_BUY_RATE;
+    public static String UI_USD_SALE_RATE;
+    public static String UI_EUR_BUY_RATE;
+    public static String UI_EUR_SALE_RATE;
+}
