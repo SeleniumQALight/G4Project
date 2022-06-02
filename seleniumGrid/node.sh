72bde98b77b2956e0959bc80911c3aa87b66288b
loopchoker <fastmotionsg@gmail.com>
2022-06-02 21:25:40 +0300

    restore path

--- seleniumGrid/node.sh
+++ seleniumGrid/node.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar selenium-server-standalone-2.44.0.jar -role node -nodeConfig nodeconfig.json -Dwebdriver.chrome.driver="/Users/iuada0s2/Documents/projects/loopchoker/G4Project/drivers/102/chromedriver_mac64_m1/chromedriver"
+java -jar selenium-server-standalone-2.44.0.jar -role node -nodeConfig nodeconfig.json -Dwebdriver.chrome.driver="..\drivers\102\chromedriver_mac64\chromedriver"
 sleep 10000
